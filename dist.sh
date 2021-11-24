db5974c7ce2d95fcbee37134ca978e6a3d57791d
Sam Parton <sam@idevelopthings.com>
2021-11-24 03:18:35 +0000

    Built assets ready for publishing

--- dist.sh
+++ dist.sh
@@ -1,8 +1,5 @@
 rm -R dist
 yarn build-package
-cp package.json dist/package.json
-cp -R bin dist/bin
-cp README.md dist/README.md
 
 git add .
 git commit -m "Built assets ready for publishing"
@@ -12,14 +9,9 @@ cp package.json dist/package.json
 
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 
-git add package.json
-git add dist
+git .
 git commit -m "Increment package version to $PACKAGE_VERSION"
 
-cd dist || exit
-
 npm publish --access public
-
-cd ../ || exit
 
 git push origin main
